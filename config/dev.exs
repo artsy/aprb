ae20719ba99963b4cc579841f4c4e078c51885e2
ashkan <ashkan@artsymail.com>
2017-02-16 16:06:54 -0500

    Cleanup kafka...

--- config/dev.exs
+++ config/dev.exs
@@ -1,11 +1,5 @@
 use Mix.Config
 
-config :kafka_ex,
-  brokers: [{"kafka-0.stg.artsy.systems", 9092}],
-  consumer_group: System.get_env("KAFKA_CONSUMER_GROUP") || "kafka_ex_2_local",
-  disable_default_worker: false,
-  sync_timeout: 1000 #Timeout used synchronous requests from kafka. Defaults to 1000ms.
-
 config :maru, Aprb.Api.Root,
   http: [port: 4000]
 
