5536e33d5d8ae0d7893739387c84c1986e5e5994
ashkan <ashkan@artsymail.com>
2017-05-02 16:28:30 -0400

    More readable verb for initial subscriptions

--- lib/helpers/subscription_helper.ex
+++ lib/helpers/subscription_helper.ex
@@ -1,6 +1,6 @@
 defmodule Aprb.SubscriptionHelper do
   def parsed_verb(event) do
     initial = if event["properties"]["partner"]["initial_subscription"], do: 'initial', else: '' 
-    "#{event["verb"]}#{initial}"
+    "#{event["verb"]}-#{initial}"
   end
 end
