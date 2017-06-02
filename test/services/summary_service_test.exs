cb1d67af6cdadf04e7beb8e78e0d0fd680282774
ashkan <ashkan@artsymail.com>
2017-06-02 15:50:45 -0400

    Fix tests

--- test/services/summary_service_test.exs
+++ test/services/summary_service_test.exs
@@ -2,7 +2,7 @@ defmodule Aprb.Service.SummaryServiceTest do
   use ExUnit.Case, async: true
   import Aprb.Factory
   alias Aprb.{Repo, Summary, Service.SummaryService}
-  
+
   setup do
     Ecto.Adapters.SQL.Sandbox.checkout(Repo)
     Ecto.Adapters.SQL.Sandbox.mode(Aprb.Repo, { :shared, self() })
@@ -10,15 +10,20 @@ defmodule Aprb.Service.SummaryServiceTest do
     :ok
   end
 
-  test "update_summary: users" do
-    topic = insert(:topic, name: "users")
+  test "update_summary: inquiries" do
+    topic = insert(:topic, name: "inquiries")
     event = %{
                "subject" => %{"display" => "Best collector"},
-               "verb" => "followed",
+               "verb" => "inquired",
                "properties" => %{
-                  "artist" => %{
-                    "id" => "test-artist"
-                  }
+                  "inquireable" => %{
+                    "id" => "artwork_1"
+                  },
+                  "inquirer" => %{
+                    "professional_buyer" => true,
+                    "confirmed_buyer" => false
+                  },
+                  "initial_message_snippet" => "this is a test"
                }
              }
     assert Repo.aggregate(Summary, :count, :verb) == 0
@@ -26,7 +31,7 @@ defmodule Aprb.Service.SummaryServiceTest do
     assert Repo.aggregate(Summary, :count, :verb) == 1
     summary = Repo.one(Summary)
     assert summary.topic_id == topic.id
-    assert summary.verb == "followed"
+    assert summary.verb == "inquired"
     assert summary.total_count == 1
 
     # sending event again will add total_count in summary
@@ -35,7 +40,7 @@ defmodule Aprb.Service.SummaryServiceTest do
     assert Repo.aggregate(Summary, :count, :verb) == 1
     summary = Repo.one(Summary)
     assert summary.topic_id == topic.id
-    assert summary.verb == "followed"
+    assert summary.verb == "inquired"
     assert summary.total_count == 2
   end
 end
