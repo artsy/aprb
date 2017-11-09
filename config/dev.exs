66069d48b2b7f0ff1a1be7cb795c75f882540dd4
Jon Allured <jon.allured@gmail.com>
2017-11-09 11:04:15 -0500

    Specify fewer things for dev setup
    While getting this setup on my machine, @ashkan18 and I made this
    change.

--- config/dev.exs
+++ config/dev.exs
@@ -7,6 +7,6 @@ config :aprb, Aprb.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: System.get_env("DB_USER"),
   password: System.get_env("DB_PASSWORD"),
-  database: System.get_env("DB_NAME"),
-  hostname: System.get_env("DB_HOST"),
+  database: "aprb_dev",
+  hostname: "localhost",
   pool_size: 10
