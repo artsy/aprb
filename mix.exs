33c1965a514d1722cb898f2396390969323dd28b
ashkan18 <ashkan18@gmail.com>
2018-10-12 22:43:16 -0400

    Add jason

--- mix.exs
+++ mix.exs
@@ -25,6 +25,7 @@ defmodule Aprb.Mixfile do
     [ {:maru, "~> 0.12"},
       {:amqp, "0.2.2"},
       {:poison, "~> 3.0", override: true},
+      {:jason, "~> 1.1"},
       {:slack, "~> 0.11.0"},
       {:postgrex, ">= 0.0.0"},
       {:ecto, "~> 2.0.0"},
