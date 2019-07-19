83de9352488acc94f44a176c925f7a6a03facd79
ashkan <ashkan@artsymail.com>
2019-07-19 14:49:31 -0400

    Fix wrong buyer link

--- lib/views/commerce/commerce_order_slack_view.ex
+++ lib/views/commerce/commerce_order_slack_view.ex
@@ -76,7 +76,7 @@ defmodule Aprb.Views.CommerceOrderSlackView do
       },
       %{
         title: "Buyer",
-        value: "<#{exchange_user_orders_link(buyer["_id"])}|#{cleanup_name(buyer["name"])}>",
+        value: "<#{exchange_user_orders_link(buyer["id"])}|#{cleanup_name(buyer["name"])}>",
         short: true
       },
       %{
